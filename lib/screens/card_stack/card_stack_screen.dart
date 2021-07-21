b94cfc8e7a0ee0c169d2813d101031b0d8a1958d
Andres Osorio <af.osorio1341@gmail.com>
2021-07-21 07:50:27 -0500

    Upgrade Flutter version

--- lib/screens/card_stack/card_stack_screen.dart
+++ lib/screens/card_stack/card_stack_screen.dart
@@ -16,6 +16,21 @@ class CardStackScreen extends StatefulWidget {
 }
 
 class _CardStackScreenState extends State<CardStackScreen> {
+  List<Widget> _cardList = [];
+
+  void _removeCard(index) {
+    setState(() {
+      _cardList.removeAt(index);
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _cardList = _getMatchCard();
+  }
+
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
@@ -24,7 +39,14 @@ class _CardStackScreenState extends State<CardStackScreen> {
       builder: (BuildContext context, AsyncSnapshot snap) {
         if (snap.hasData) {
           print(snap.data);
-          return Scaffold();
+          return Scaffold(
+            body: Center(
+              child: Stack(
+                alignment: Alignment.center,
+                children: _cardList,
+              ),
+            ),
+          );
         } else {
           return Center(
             child: Column(
@@ -41,5 +63,44 @@ class _CardStackScreenState extends State<CardStackScreen> {
         }
       },
     );
+  }
+
+  List<Widget> _getMatchCard() {
+    List<Widget> cardList = [];
+
+    for (int x = 0; x < 3; x++) {
+      cardList.add(Positioned(
+        top: (x + 1) * 10,
+        child: Draggable(
+          onDragEnd: (drag) {
+            print(drag);
+            _removeCard(x);
+          },
+          childWhenDragging: Container(),
+          feedback: Card(
+            elevation: 12,
+            color: Colors.grey[200],
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            child: Container(
+              width: 240,
+              height: 300,
+            ),
+          ),
+          child: Card(
+            elevation: 12,
+            color: Colors.grey[200],
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            child: Container(
+              width: 240,
+              height: 300,
+            ),
+          ),
+        ),
+      ));
+    }
+
+    return cardList;
   }
 }
